402a3e6f79acfaaf8a07aed8efcfd553bf9a1abe
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-09 22:57:23 +0300

    seminar 1

---
+++ task0/Program.cs
@@ -0,0 +1,7 @@
+﻿//Написать программу, кот на вход принимает число и выдает его квадрат
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int quater = number * number;
+
+Console.WriteLine($"Квадрат числа {number} равен {quater}");
