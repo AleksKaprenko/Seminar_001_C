d0c85e97f08611168ef1aaf7883b286c9525902d
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-13 23:58:05 +0300

    add task10_DZ

---
+++ Seminar1_DZ/task10_DZ/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int SecondDigit(int num2)
+{
+    int digit2=num2%100/10;
+    return digit2;
+}
+
+Console.WriteLine(); // выводит доп. пустую строку
+
+Start:
+Console.Write("Введите 3-значное число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+   if (N>99 && N<1000)
+    {
+      System.Console.Write("Вторая цифра введеного числа равна ");
+      System.Console.WriteLine(SecondDigit(N));
+    }
+    else 
+    {
+        System.Console.WriteLine("Введенное число не является 3-значным. Повторите ввод.");
+        System.Console.WriteLine();
+        goto Start;
+    }
+
