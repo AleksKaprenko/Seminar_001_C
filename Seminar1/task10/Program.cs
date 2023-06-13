d027c8a91f46cf1100523e84b571db33f41a2320
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-13 22:31:49 +0300

    add task9 + task10

---
+++ Seminar1/task10/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+int SecondDigitDelete(int num)
+{
+int digit1 = num / 100;
+int digit3 = num % 10;
+int result=(digit1*10)+digit3;
+return result;
+}
+
+int number = new Random().Next(100, 1000);
+/*
+int digit1 = number / 100;
+int digit3 = number % 10;
+int result=(digit1*10)+digit3;
+*/
+Console.WriteLine($"Случайное число: {number}");
+//Console.WriteLine($"{digit1}{digit3}");
+//Console.WriteLine(result);
+Console.WriteLine(SecondDigitDelete(number));
---
+++ Seminar1/task9/Program.cs
@@ -0,0 +1,21 @@
+﻿// программа выводит случайное число из (10, 99)  и показывает наибольшую цифру числа
+ int DigitComparision(int num)
+ {
+    int digit1 = num /10;
+    int digit2 = num%10;
+    int result = 0;
+    if (digit1>digit2)
+    {
+        result = digit1;
+    }
+    else 
+    {
+        result = digit2;
+    }
+    return result;
+ }
+
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число: {number}");
+Console.WriteLine($"Макс цифра равна: {DigitComparision(number)}");
