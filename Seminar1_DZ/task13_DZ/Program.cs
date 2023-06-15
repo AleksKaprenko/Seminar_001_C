a06230f2fdf5907297c8fa6c21172ef059fccebc
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-15 23:14:50 +0300

    add task13_DZ

---
+++ Seminar1_DZ/task13_DZ/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+void PrintThirdDigit(string num)
+{
+    if (num.Length>2)
+    {
+        System.Console.Write($"Третья цифра введеного числа равна {num[2]}");
+    }
+    else
+    {
+        System.Console.WriteLine("Введенное число не является 3-значным. Повторите ввод.");
+    }
+}
+
+
+Console.Write("Введите 3-значное число: ");
+string N = Console.ReadLine();
+N = String.Join("", N.Split('-', ',' ,'.'));
+PrintThirdDigit(N);
