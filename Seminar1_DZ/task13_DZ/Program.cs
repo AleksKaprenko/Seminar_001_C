b49115ef9c2d07c19fe1c13207a416e5aa9ff88b
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-16 14:35:02 +0300

    code with stopwatch

--- Seminar1_DZ/task13_DZ/Program.cs
+++ Seminar1_DZ/task13_DZ/Program.cs
@@ -3,12 +3,17 @@
 78 -> третьей цифры нет
 32679 -> 6
 */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
 
 void PrintThirdDigit(string num)
 {
     if (num.Length>2)
     {
-        System.Console.Write($"Третья цифра введеного числа равна {num[2]}");
+        System.Console.WriteLine($"Третья цифра введеного числа равна {num[2]}");
     }
     else
     {
@@ -16,8 +21,14 @@ void PrintThirdDigit(string num)
     }
 }
 
-
 Console.Write("Введите 3-значное число: ");
-string N = Console.ReadLine();
+string? N = Console.ReadLine();
+if (N!=null)
+{
+var sw = new Stopwatch();
+sw.Start();
 N = String.Join("", N.Split('-', ',' ,'.'));
 PrintThirdDigit(N);
+sw.Stop();
+Console.WriteLine($"Время выполнения - {sw.Elapsed}");
+}
---
+++ Seminar1_DZ/task13_DZ/Task13_DZ_classes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+/*namespace task13_DZ
+{
+    public class Task13_DZ_classes
+    {
+        
+    }
+}
+*/
