a8a15c698882cf422f11ac85a58da669d0bd8d39
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-16 21:47:24 +0300

    add task17, 21, 22

---
+++ Seminar1/task18/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+
+string[] quaterarray = {"x>0 , y>0" , "x<0 , y>0" , "x>0 , y>0" , "x<0 , y>0"};
+Start:
+Console.WriteLine("Введите номер четверти:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x>=1 && x<=4)
+{
+Console.WriteLine(quaterarray[x-1]);
+}
+else 
+{
+    Console.WriteLine("Введен неверный номер четверти.");
+    goto Start;
+}
