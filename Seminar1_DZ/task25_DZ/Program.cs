0c12cedd28b71149a873d70836bb6aa4d93da9b4
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-20 23:46:16 +0300

    add task25_DZ_exponential A of B

---
+++ Seminar1_DZ/task25_DZ/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+double Exponential(double x, double y)
+{
+    double result = Math.Pow(x, y);
+    return result;
+}
+
+System.Console.Write("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"Число {num1} в степени {num2} = {Exponential(num1, num2)}");
