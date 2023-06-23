15d6cc205a1398bf51d489245498761a7623d07e
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-24 00:41:22 +0300

    task32_RevertArray

---
+++ Seminar1/task32/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 32: Напишите программу замена элементов массива: 
+положительные элементы замените на соответствующие отрицательные, и наоборот.
+[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+*/
+
+int[] FillArrayRandom(int size) 
+{
+int[] array = new int[size]; 
+Random rnd = new Random();
+for (int i = 0; i<size; i++)
+    {
+      array[i] = rnd.Next(-99, 100);
+    }
+    return array;
+}
+
+int[] ArrayRevert(int[] array)
+{
+    int[] NewArr = new int[array.Length];
+    for (int i=0; i<array.Length; i++)
+    {
+        NewArr[i] = -array[i];
+    }
+    return NewArr;
+}
+
+System.Console.Write("Укажите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayRandom(length);
+System.Console.WriteLine($"Введен массив: [{string.Join("; ", array)}]");
+int[] NewArr = ArrayRevert(array);
+System.Console.WriteLine($"перевернутый массив: [{string.Join("; ", NewArr)}]");
