dc0b9b0ed9388a029d3953e1c066f90f387f1287
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-24 19:12:00 +0300

    task36_DZ: Sum.Odd.Elements.inArray

---
+++ Seminar1_DZ/task36_DZ/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] FillArrayRandom(int size) //vметод заполнения массива случайными целыми числами
+{
+int[] array = new int[size]; 
+Random rnd = new Random();
+for (int i = 0; i<size; i++)
+    {
+      array[i] = rnd.Next(-99, 100); // диапазон генерируемых случайных целых
+    }
+    return array;
+}
+
+int SummOddElements(int[] array)
+{
+        int summ=0;
+        for (int i=0; i<array.Length; i++)
+            if (i%2==1) // определение нечетной позиции элемента массива. Первый элемент имеет позицию 0 (четная)
+            {
+               summ+=array[i]; 
+            }
+        return summ;
+}
+
+System.Console.Write("Укажите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayRandom(length);
+System.Console.WriteLine($"\nВведен массив: \n[{string.Join("; ", array)}]\n");
+int summ = SummOddElements(array);
+System.Console.WriteLine($"Сумма элементов нечетных позиций равна {summ}");
