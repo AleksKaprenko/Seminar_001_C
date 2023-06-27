6610561eb660f0fa981eb0c90561047d69def77d
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-27 22:41:15 +0300

    task42: DecimalToBinary

---
+++ Seminar1/task42/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3  -> 11
+2  -> 10
+*/
+
+void DecToBinary(int a) // рекурсия
+{
+   if (a == 0)
+    {
+        return;
+    }
+    DecToBinary(a/2);
+    System.Console.Write(a%2);    
+}
+
+System.Console.Write("Введите десятичное число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+DecToBinary(a);
+System.Console.WriteLine();
+System.Console.Write(Convert.ToString(a, 2)); // преобразование int в строку - двоичное число
