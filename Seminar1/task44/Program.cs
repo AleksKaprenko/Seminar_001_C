0ae47feac646b57790c885b54c5879a242a187ef
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-27 22:54:17 +0300

    task42: Fibonachi

---
+++ Seminar1/task44/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+Если N = 5 -> 0 1 1 2 3
+Если N = 3 -> 0 1 1
+Если N = 7 -> 0 1 1 2 3 5 8
+*/
+
+int[] Fibo(int a)
+{
+    int[] fibo = new int[a];
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for (int i=2; i<a; i++)
+            {
+                fibo[i] = fibo[i-1]+fibo[i-2];
+            }
+        return fibo;
+}
+
+System.Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>=2)
+{
+System.Console.Write($"Последовательность Фибоначчи числа {a}: {string.Join(", ", Fibo(a))}");
+}
+else if (a==1) System.Console.Write($"Последовательность Фибоначчи числа {a}: 0, 1");
+else System.Console.WriteLine("Ошибка ввода");
