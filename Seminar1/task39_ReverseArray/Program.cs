61c2f31c02ab2576506c6a6e60accb362ac88e78
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-27 22:03:21 +0300

    task39_ReverseArray

---
+++ Seminar1/task39_ReverseArray/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 39: Напишите программу, которая перевернёт одномерный массив 
+(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+*/
+
+int[] FillArrayRandom(int size) 
+{
+int[] array = new int[size]; 
+Random rnd = new Random();
+for (int i = 0; i<size; i++)
+    {
+      array[i] = rnd.Next(0, 100);
+    }
+    return array;
+}
+
+int[] ReverseArray(int[] arr)
+{
+    int temp = 0;
+    for (int i=0; i<arr.Length/2; i++)
+    {
+        temp = arr[i];
+        arr[i] = arr[arr.Length-1-i];
+        arr[arr.Length-1-i] = temp;
+    }
+    return arr;
+}
+
+System.Console.Write("Укажите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayRandom(length);
+System.Console.WriteLine($"Введен массив: [{string.Join("; ", array)}]");
+System.Console.WriteLine($"Перевернутый массив: [{string.Join("; ", ReverseArray(array))}]");
