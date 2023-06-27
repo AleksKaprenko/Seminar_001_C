6d67079e75f0ef5d6e7f1cf68d470d2063d95d69
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-27 23:19:42 +0300

    task38 add v.2 random double

--- Seminar1_DZ/task38_DZ/Program.cs
+++ Seminar1_DZ/task38_DZ/Program.cs
@@ -7,30 +7,22 @@ double[] FillArrayRandomDouble(int size) // метод заполнения ма
 {
 double[] array = new double[size]; 
 Random rnd = new Random();
-double minRange = -50; //  мин. граница генерируемых чисел, задаем произвольно
-double maxRange = 50; // макс. граница генерируемых чисел, задаем произвольно
+//double minRange = -100, maxRange = 100; // для вар 1:  мин. и max граница генерируемых чисел, задаем произвольно
 for (int i = 0; i<size; i++)
     {
-    //  array[i] = Math.Round((rnd.NextDouble() *(maxRange-minRange) + minRange), 2); // random в диапазоне (minRange, maxRange) с округлением до 2 знаков
-  array[i] = Math.Round((rnd.NextDouble() + rnd.Next(0, 10)), 2); // вариант 2
+   // array[i] = Math.Round((rnd.NextDouble() *(maxRange-minRange) + minRange), 2); // вар 1: random в диапазоне (minRange, maxRange) с округлением до 2 знаков
+  array[i] = Math.Round((rnd.NextDouble() + rnd.Next(-100, 100)), 3); // вариант 2 random
     }
     return array;
 }
 
 double MaxSubMin(double[] array)
 {
-        double max=array[0];
-        double min=array[0];;
+        double max=array[0], min=array[0];
         for (int i=1; i<array.Length; i++)
-            if (array[i]>max) 
-            {
-               max=array[i]; 
-            }
-            else if (array[i]<min)
-            {
-               min=array[i]; 
-            }
-        return (Math.Round((max-min), 2));
+            if (array[i]>max) max=array[i]; 
+            else if (array[i]<min) min=array[i]; 
+        return (max-min);
 }
 
 System.Console.Write("Укажите размер массива: ");
@@ -39,7 +31,7 @@ double[] array = FillArrayRandomDouble(length);
 System.Console.WriteLine($"\nВведен массив: \n[{string.Join("; ", array)}]\n");
 
 //вариант 1 с использованием метода для поиска макс. и мин. в цикле и вычисления разницы
-double sub = MaxSubMin(array);
+double sub = Math.Round(MaxSubMin(array), 2);
 System.Console.WriteLine($"Вариант кода 1 (поиск мин и макс в цикле). Разница между max и min равна {sub}\n");
 
 //вариант 2 без доп. метода используя встроенные функции массива
