d92b851df4ff571fb88c3879325939792f024c51
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-06-30 10:14:16 +0300

    task41_DZ: CountPositiveNumbers

---
+++ Seminar1_DZ/task41_DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+
+int CountPositiveNumbers(int value)
+{
+    int count = 0;
+    for (int i = 1; i <= value; i++)
+    {
+        System.Console.Write($"Введите целое число #{i}: ");
+        //int number = Convert.ToInt32(Console.ReadLine());
+        if (Convert.ToInt32(Console.ReadLine()) > 0) count++;
+    }
+    return count;
+}
+
+System.Console.Write("Укажите количество вводимых чисел: ");
+int number = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($"Ведено чисел больше 0: {CountPositiveNumbers(number)}");
+
