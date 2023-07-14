4a4cc8532d92b6bd325db020f98b4a74a6872f32
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-07-14 23:08:24 +0300

    change matrix elements with even indexes by their ^2

---
+++ Seminar1/task49/Program.cs
@@ -0,0 +1,49 @@
+﻿/*Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+*/
+
+void FillMatrixWithRandom(int[,] matrix)
+{
+    Random rnd = new Random();
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            matrix[i,j]=rnd.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i,j]} \t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+void QuadValue(int[,] matrix)
+{
+    for (int i=0; i<matrix.GetLength(0); i++)
+    {
+        for (int j=0; j<matrix.GetLength(1); j++)
+        {
+            if (i%2==0 && j%2==0) matrix[i,j] *=  matrix[i,j];
+        }
+       
+    }
+}
+
+System.Console.Write("Укажите количество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Укажите количество столбцов: ");
+int column = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[row, column];
+FillMatrixWithRandom(matrix);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+QuadValue(matrix);
+PrintMatrix(matrix);
