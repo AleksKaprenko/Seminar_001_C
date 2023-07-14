aa32844321adf42ebe33782199784c9983395ecc
aleks.karpenko <aleks.karpenko.108@gmail.com>
2023-07-14 23:22:34 +0300

    recursion: print numbers from M to N

---
+++ Seminar1/task65/Program.cs
@@ -0,0 +1,21 @@
+﻿// // Задача 65: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+// // M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// // M = 4; N = 8 -> "4, 6, 7, 8"
+
+void numbers(int m, int n)
+{
+    if (n == m)
+    {
+        System.Console.Write($"{n} ");
+        return;
+    }
+    numbers(m, n-1);
+    System.Console.Write($"{n} ");
+}
+
+System.Console.Write("Введите целое число M: ");
+int M = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите целое число N > M: ");
+int N = Convert.ToInt32(Console.ReadLine());
+numbers(M, N);
